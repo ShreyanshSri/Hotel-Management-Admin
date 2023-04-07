b69fff506e429ab9680f3407890267aa6fb8800e
Shreyansh <Shreyanshsri2812@gmail.com>
2023-04-07 10:55:31 +0530

    Change

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let btnA = document.getElementById("btn1");
-let money;
-dataObj = [];
+ money = 0;
+
 btnA.addEventListener("click" , function(e)
 {
     document.getElementById("form1").style.display = 'inline';
@@ -22,42 +22,73 @@ btnC.addEventListener("click" , function(e)
 })
 
 let submitform = document.getElementById("formSubmit");
-// let myobj;
-submitform.addEventListener("click" , function(e)
-{
-    let name = document.getElementById("name");
-    let email = document.getElementById("email");
-    let phone = document.getElementById("phone");
-    let checkIn = document.getElementById("check-in");
-    let checkOut = document.getElementById("check-out");
-    
-     let myobj = {
-        name_val : name.value,
-        email_val : email.value,
-        phone_val : phone.value,
-        booking : checkIn.value,
-        leave: checkOut.value ,
-        rate : money
 
+submitform.addEventListener("click", function(e){
+    let addName = document.getElementById("name");
+    let addemail = document.getElementById("email");
+    let addphone = document.getElementById("phone");
+    let arrival_date = document.getElementById("check-in");
+    let leave_date = document.getElementById("check-out");
+
+      
+
+    let data = {
+        name: addName.value,
+        email: addemail.value,
+        phone: addphone.value,
+        checkIn: arrival_date.value,
+        checkOut: leave_date.value
     };
-    dataObj.push(myobj);
-    dataObj.push(myobj);
-    localStorage.setItem("values", JSON.stringify(dataObj));
-    let val = localStorage.getItem("values");
-    console.log(val);
 
+    
+    let val = phone.value;
+    localStorage.setItem(val , JSON.stringify(data));
+    // console.log(valObj);
 })
+myObj = [];
+const myarr = [];
+function showInfo()
+{ 
+    let html = " ";
+   
+   for(x in localStorage)
+   {
+    console.log(x);
+    //  element = localStorage.getItem(x);
+    //  myarr.push(element);
 
-console.log(values);
+   }
 
-// let arr = [];
-// arr.push(val);
-// dataObj.push(val);
-
-
-// localStorage.getItem("values")
-// values.forEach(element => {
-//     console.log(element);
-// });
-
-let html = '';
+ 
+   myarr.forEach(function(element)
+   {
+    html += `
+    <div class="card">
+                    
+    <div class="card_content">
+         <h5>Name : ${element.name}</h5>
+    </div>
+    <div class="card_content">
+        <h5>Emai : ${element.email}</h5>
+   </div>
+   <div class="card_content">
+    <h5>Phone : ${element.phone}</h5>
+   </div>
+   <div class="card_content">
+    <h5>Check-in : ${element.checkIn}</h5>
+   </div>
+   <div class="card_content">
+    <h5>check-out : ${element.checkOut}</h5>
+   </div>
+   <div id="btn">
+    <div class="btn" id=""><button>Edit</button></div>
+    <div class="btn" id=""><button>Delete</button></div>
+   </div>
+   
+</div>
+    `
+   })  
+        
+}
+console.log(myarr);
+showInfo();
